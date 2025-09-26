e797419f885f8f19898e3550683698d24f8c1a96
Daniil <daniil@hotmail.com>
2025-09-26 11:51:55 +0300

    Add visualizer v0.1, fixed resizing

--- apps/calc/calc.js
+++ apps/calc/calc.js
@@ -4,10 +4,10 @@ const modeSelector = document.getElementById("chooseMode");
 
 //default mode
 window.addEventListener("DOMContentLoaded", function() {
-  const template = document.getElementById("basicMode-template");
+  const template = document.getElementById("advancedMode-template");
   const clone = template.content.cloneNode(true);
   document.getElementById("calculator-flex").appendChild(clone);
-  basicModeScript();
+  advancedModeScript();
 })
 
 //switching mode with radioButton
@@ -15,18 +15,11 @@ modeSelector.addEventListener("change", function (e){
   let nextTemplate;
   let cloneTemplate;
   display.value = "";
-  const currentMode = mainContainer.querySelector("#basicMode-container, #advancedMode-container, #pythMode-container");
+  const currentMode = mainContainer.querySelector("#advancedMode-container, #pythMode-container");
   if (currentMode){
     currentMode.remove();
   }
-
-  if (e.target.value == "basic"){
-    nextTemplate = document.getElementById("basicMode-template");
-    cloneTemplate = nextTemplate.content.cloneNode(true);
-    document.getElementById("calculator-flex").appendChild(cloneTemplate);
-    basicModeScript();
-  }
-  else if (e.target.value == "advanced"){
+  if (e.target.value == "advanced"){
     nextTemplate = document.getElementById("advancedMode-template");
     cloneTemplate = nextTemplate.content.cloneNode(true);
     document.getElementById("calculator-flex").appendChild(cloneTemplate);
@@ -40,53 +33,8 @@ modeSelector.addEventListener("change", function (e){
   }
 });
 
-//Some math functions
-function summary(a, b){
-  return a+b;
-}
-function subtraction(a, b){
-  return a-b;
-}
-function multiplication(a, b){
-  return a*b;
-}
-function divison(a, b){
-  if (b == 0){
-    return "Division by 0!";
-  }
-  return a/b;
-}
 function hypotenuse(a, b){
   return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-}
-
-
-
-//Basic Mode
-function basicModeScript(){
-  const container = document.getElementById("basicMode-container");
-  let basicModeButtons = container.querySelectorAll(".button");
-
-  for (let i = 0; i < basicModeButtons.length; i++){
-    let button = basicModeButtons[i];
-
-    button.addEventListener("click", function (){
-      const valueA = Number(document.getElementById("inputNumberA").value);
-      const valueB = Number(document.getElementById("inputNumberB").value);
-      if (button.value == "sum"){
-        display.value = summary(valueA, valueB);
-      }
-      else if (button.value == "subtr"){
-        display.value = subtraction(valueA, valueB);
-      }
-      else if (button.value == "mult"){
-        display.value = multiplication(valueA, valueB);
-      }
-      else if (button.value == "div"){
-        display.value = divison(valueA, valueB);
-      }
-    });
-  }
 }
 
 //AdvancedMode
@@ -134,8 +82,13 @@ function pythModeScript(){
   button.addEventListener("click", function (){
     const valueA = Number(document.getElementById("inputSideA").value);
     const valueB = Number(document.getElementById("inputSideB").value);
-    display.value = hypotenuse(valueA,valueB);
-    explanation.innerHTML = valueA + "&sup2; + " + valueB + "&sup2; = " + (Math.pow(valueA, 2) + Math.pow(valueB, 2)) + ". &radic;" + (Math.pow(valueA, 2) + Math.pow(valueB, 2)) + " is the hypotenuse's length."
+    if(valueA < 1 || valueB < 1){
+      display.value = "Invalid input!"
+    }
+    else{
+      display.value = hypotenuse(valueA,valueB);
+      explanation.innerHTML = valueA + "&sup2; + " + valueB + "&sup2; = " + (Math.pow(valueA, 2) + Math.pow(valueB, 2)) + ". &radic;" + (Math.pow(valueA, 2) + Math.pow(valueB, 2)) + " is the hypotenuse's length."
+    }
   });
 }
 
