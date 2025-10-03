4903cc23b7953e680a9afc2df25e9e9e573f653b
Daniil <daniil@hotmail.com>
2025-10-03 15:00:18 +0300

    Calculator's fixes (display limit, code optimization) + formatter

--- apps/calc/calc.js
+++ apps/calc/calc.js
@@ -3,93 +3,105 @@ const mainContainer = document.getElementById("calculator-flex");
 const modeSelector = document.getElementById("chooseMode");
 
 //default mode
-window.addEventListener("DOMContentLoaded", function() {
-  const template = document.getElementById("advancedMode-template");
-  const clone = template.content.cloneNode(true);
-  document.getElementById("calculator-flex").appendChild(clone);
-  advancedModeScript();
-})
-
-//switching mode with radioButton
-modeSelector.addEventListener("change", function (e){
-  let nextTemplate;
-  let cloneTemplate;
-  display.value = "";
-  const currentMode = mainContainer.querySelector("#advancedMode-container, #pythMode-container");
-  if (currentMode){
-    currentMode.remove();
-  }
-  if (e.target.value == "advanced"){
-    nextTemplate = document.getElementById("advancedMode-template");
-    cloneTemplate = nextTemplate.content.cloneNode(true);
-    document.getElementById("calculator-flex").appendChild(cloneTemplate);
-    advancedModeScript();
-  }
-  else if (e.target.value == "pyth"){
-    nextTemplate = document.getElementById("pythMode-template");
-    cloneTemplate = nextTemplate.content.cloneNode(true);
-    document.getElementById("calculator-flex").appendChild(cloneTemplate);
-    pythModeScript();
-  }
+window.addEventListener("DOMContentLoaded", function () {
+  switchMode("advancedMode-template");
 });
 
-function hypotenuse(a, b){
+//switching mode with radioButton
+modeSelector.addEventListener("change", function (e) {
+  setDisplayValue("");
+  const currentMode = mainContainer.querySelector(
+    "#advancedMode-container, #pythMode-container"
+  );
+
+  currentMode.remove();
+  switchMode(e.target.value);
+});
+
+function switchMode(template) {
+  let nextTemplate = document.getElementById(template);
+  let cloneTemplate = nextTemplate.content.cloneNode(true);
+  document.getElementById("calculator-flex").appendChild(cloneTemplate);
+  if (template == "advancedMode-template") {
+    advancedModeScript();
+  } else {
+    pythModeScript();
+  }
+}
+
+function setDisplayValue(value) {
+  const strValue = String(value);
+  const maxDisplayLength = 9;
+  if (strValue.length > maxDisplayLength) {
+    display.value = strValue.slice(0, maxDisplayLength);
+  } else {
+    display.value = strValue;
+  }
+}
+
+function hypotenuse(a, b) {
   return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 }
 
 //AdvancedMode
-function advancedModeScript(){
+function advancedModeScript() {
   const container = document.getElementById("advancedMode-container");
   let advancedModeButtons = container.querySelectorAll(".button");
-  for (let i = 0; i < advancedModeButtons.length; i++){
+  for (let i = 0; i < advancedModeButtons.length; i++) {
     let button = advancedModeButtons[i];
 
-    button.addEventListener("click", function(){
-      if(button.classList.contains("button-num") || button.classList.contains("button-operator")){
+    button.addEventListener("click", function () {
+      if (
+        (button.classList.contains("button-num") ||
+          button.classList.contains("button-operator")) &&
+        display.value.length < 12
+      ) {
         display.value += button.value;
-      }
-      else if(button.classList.contains("button-clear")){
-        display.value="";
-      }
-      else if(button.classList.contains("button-result")){
-        if(eval(display.value) == "Infinity" || eval(display.value) == "-Infinity"){
-          display.value = "Division by 0!"
+      } else if (button.classList.contains("button-clear")) {
+        setDisplayValue("");
+      } else if (button.classList.contains("button-result")) {
+        if (
+          eval(display.value) == "Infinity" ||
+          eval(display.value) == "-Infinity"
+        ) {
+          setDisplayValue("Error!");
+        } else {
+          setDisplayValue(eval(display.value));
         }
-        else{
-          display.value = eval(display.value);
-        }
-      }
-      else if(button.id == "buttonPow2"){
-        display.value = eval(Math.pow(display.value, 2));
-      }
-      else if(button.id == "buttonSqrt"){
-        display.value = eval(Math.sqrt(display.value));
-      }
-      else if(button.id == "button-back"){
-        display.value = display.value.slice(0, -1);
+      } else if (button.id == "buttonPow2") {
+        setDisplayValue(eval(Math.pow(display.value, 2)));
+      } else if (button.id == "buttonSqrt") {
+        setDisplayValue(eval(Math.sqrt(display.value)));
+      } else if (button.id == "button-back") {
+        setDisplayValue(display.value.slice(0, -1));
       }
     });
   }
 }
 
 //PythMode
-function pythModeScript(){
+function pythModeScript() {
   const container = document.getElementById("pythMode-container");
   const buttonList = container.querySelectorAll(".button");
   const explanation = container.querySelector("#explanation");
   let button = buttonList[0];
-  
-  button.addEventListener("click", function (){
+
+  button.addEventListener("click", function () {
     const valueA = Number(document.getElementById("inputSideA").value);
     const valueB = Number(document.getElementById("inputSideB").value);
-    if(valueA < 1 || valueB < 1){
-      display.value = "Invalid input!"
-    }
-    else{
-      display.value = hypotenuse(valueA,valueB);
-      explanation.innerHTML = valueA + "&sup2; + " + valueB + "&sup2; = " + (Math.pow(valueA, 2) + Math.pow(valueB, 2)) + ". &radic;" + (Math.pow(valueA, 2) + Math.pow(valueB, 2)) + " is the hypotenuse's length."
+    if (valueA <= 0 || valueB <= 0) {
+      setDisplayValue("Error!");
+    } else {
+      setDisplayValue(hypotenuse(valueA, valueB));
+      explanation.innerHTML =
+        valueA +
+        "&sup2; + " +
+        valueB +
+        "&sup2; = " +
+        (Math.pow(valueA, 2) + Math.pow(valueB, 2)) +
+        ". &radic;" +
+        (Math.pow(valueA, 2) + Math.pow(valueB, 2)) +
+        " is the hypotenuse's length.";
     }
   });
 }
-
-
--- main.js
+++ main.js
@@ -4,29 +4,27 @@ const frame = document.getElementById("app-frame");
 const mainSecHeading = document.getElementById("main-section-heading");
 const closeBtn = document.getElementById("close-btn");
 
-for (let i = 0; i < projectImages.length; i++){
+for (let i = 0; i < projectImages.length; i++) {
   let img = projectImages[i];
-  img.addEventListener("click", function(){
+  img.addEventListener("click", function () {
     frame.src = img.dataset.src;
     container.classList.add("open");
     mainSecHeading.innerHTML = img.alt;
   });
 }
 
-function closeFrame(){
+function closeFrame() {
   container.classList.remove("open");
   frame.src = "";
   mainSecHeading.innerHTML = "Interactive Apps";
 }
 
-closeBtn.addEventListener("click", function(){
+closeBtn.addEventListener("click", function () {
   closeFrame();
-})
+});
 
-addEventListener("keydown", function(e){
-  if (e.code == "Escape"){
+addEventListener("keydown", function (e) {
+  if (e.code == "Escape") {
     closeFrame();
   }
-})
-
-
+});
