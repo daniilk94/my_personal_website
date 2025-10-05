43d98490f264788d79c0d37975411b58664a67ea
Daniil <daniil@hotmail.com>
2025-10-05 19:07:33 +0300

    visualizer update 1

--- apps/sorting visualizer/sv.js
+++ apps/sorting visualizer/sv.js
@@ -1,18 +1,76 @@
 const visualizer = document.getElementById("visualizer");
-const swappingBarColor = document.getElementById("swapping-bar-color");
-const basicBarColor = document.getElementById("basic-bar-color");
 const speedBar = document.getElementById("speed-bar");
 const playButton = document.getElementById("play-button");
 const reloadButton = document.getElementById("reload-button");
 const chooseSort = document.getElementById("sorting-type");
+const muteButton = document.getElementById("mute-button");
 
 let array = generateRandomArray(40);
 let swaps = [];
+let muteSound = true;
 let isPlaying = false;
 let isPaused = false;
 let currentIndex = 0;
 let timeout = null;
 let audioCtx = null;
+let basicBarColor = "#000000";
+let swapBarColor = "#ff0000ff";
+
+//Based on documentation from https://github.com/simonwep/pickr
+
+const basicPickr = Pickr.create({
+  el: "#basic-bar-color",
+  theme: "nano",
+  default: "#000000",
+  inline: false,
+  padding: 10,
+  position: "left-end",
+  appendTo: document.body,
+  components: {
+    preview: true,
+    opacity: true,
+    hue: true,
+    interaction: {
+      input: true,
+    },
+  },
+});
+const swapPickr = Pickr.create({
+  el: "#swapping-bar-color",
+  theme: "nano",
+  default: "#ff0000ff",
+  position: "right-end",
+  padding: 20,
+  components: {
+    preview: true,
+    opacity: true,
+    hue: true,
+    interaction: {
+      input: true,
+    },
+  },
+});
+
+basicPickr.on("change", function (color) {
+  basicBarColor = color.toHEXA().toString();
+  basicPickr.applyColor();
+  const bars = visualizer.children;
+  for (const bar of bars) {
+    bar.style.backgroundColor = basicBarColor;
+  }
+});
+
+swapPickr.on("change", function (color) {
+  swapBarColor = color.toHEXA().toString();
+  swapPickr.applyColor();
+  const bars = visualizer.children;
+
+  for (const bar of bars) {
+    if (bar.dataset.isSwapping === "true") {
+      bar.style.backgroundColor = swapBarColor;
+    }
+  }
+});
 
 generateBars(array, visualizer);
 swaps = bubbleSort([...array]);
@@ -46,10 +104,14 @@ function swapChildren([left, right], container) {
   const barRight = children[right];
   container.insertBefore(barRight, barLeft);
   for (const bar of children) {
-    bar.style.backgroundColor = basicBarColor.value;
+    bar.style.backgroundColor = basicBarColor;
+    bar.dataset.isSwapping = "false";
   }
-  barLeft.style.backgroundColor = swappingBarColor.value;
-  barRight.style.backgroundColor = swappingBarColor.value;
+  barLeft.style.backgroundColor = swapBarColor;
+  barRight.style.backgroundColor = swapBarColor;
+
+  barLeft.dataset.isSwapping = "true";
+  barRight.dataset.isSwapping = "true";
 }
 
 function animate() {
@@ -63,8 +125,10 @@ function animate() {
   swapChildren(indicies, visualizer);
   currentIndex++;
 
-  playNote(50 + indicies[0] * 150);
-  playNote(50 + indicies[1] * 150);
+  if (!muteSound) {
+    playNote(50 + indicies[0] * 150);
+    playNote(50 + indicies[1] * 150);
+  }
 
   const delay = Number(speedBar.max) - Number(speedBar.value);
   timeout = setTimeout(animate, delay);
@@ -120,7 +184,7 @@ function generateBars(array, container) {
   for (let i = 0; i < array.length; i++) {
     const bar = document.createElement("div");
     bar.classList.add("bar");
-    bar.style.backgroundColor = basicBarColor.value;
+    bar.style.backgroundColor = basicBarColor;
     bar.style.height = array[i] * 100 + "%";
     container.appendChild(bar);
   }
@@ -160,6 +224,18 @@ reloadButton.addEventListener("click", function () {
   } else if (chooseSort.value === "insertion-sort") {
     swaps = insertionSort([...array]);
   }
+});
+
+muteButton.addEventListener("click", function () {
+  const muteIcon = muteButton.querySelector("i");
+  if (muteSound) {
+    muteIcon.classList.remove("fa-volume-mute");
+    muteIcon.classList.add("fa-volume-high");
+  } else {
+    muteIcon.classList.remove("fa-volume-high");
+    muteIcon.classList.add("fa-volume-mute");
+  }
+  muteSound = !muteSound;
 });
 
 chooseSort.addEventListener("change", function () {
